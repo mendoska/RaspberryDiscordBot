f41aa93b932683930e6167b063dbb281095262a0
Alex Mendoza <alexandermen2017@fau.edu>
2023-02-17 14:22:49 -0500

    created a discord hello bot!

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+#client = discord.Client(intents=intents)
+
+TOKEN = ''
+
+
+description = '''Python Discord Bot'''
+#command prefix is what you use to run the command
+
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('--------')
+
+#this runs the command
+@bot.command()
+async def hello(ctx):
+	await ctx.send('world')
+
+bot.run(TOKEN)
